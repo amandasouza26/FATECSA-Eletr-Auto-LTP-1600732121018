e201da8bace31d6b0588cdd7ddd5e58b5a1ca8a5
amandasouza26 <99343559+amandasouza26@users.noreply.github.com>
2022-02-23 12:28:21 -0300

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float e_lado, s_area;
+    printf("Entre com o valor\n");
+    scanf("%f", &e_lado);
+    s_area = e_lado * e_lado;
+    printf("%.2f\n", s_area);
+
+    return 0;
+}
