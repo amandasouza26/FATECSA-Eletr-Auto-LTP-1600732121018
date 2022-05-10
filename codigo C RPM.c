7c5267b35eaec50d3dc7279c3df69398c55843c1
amandasouza26 <99343559+amandasouza26@users.noreply.github.com>
2022-05-10 11:54:36 -0300

    Add files via upload

---
+++ codigo C RPM.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float s_saida[4][5]={{0.0,0.5,1.2,1.3,1.4},
+    {0.0,0.7,1.7,1.8,2.3},
+    {0.0,0.8,1.9,2.0,4.0},
+    {0.0,1.0,2.0,2.2,5.0}};
+    int p_rpm[5]={0,1000,2000,3000,4000};
+    float p_lambda[4]={0.1,0.2,0.3,0.4};
+    float e_lambda;
+    int e_rpm,v_var,v_rpm_pos,v_lambda_pos;
+    printf("Entre com o valor da rotação[RPM]:");
+    scanf("%i", &e_rpm);
+    printf("Entre com o valor da sonda lambda[v]:");
+    scanf("%f",&e_lambda);
+    v_rpm_pos=0;
+    v_var=0;
+    while((p_rpm[v_var]<=e_rpm)&&(v_var<5)){
+        v_rpm_pos=v_var;
+        v_var++;
+    }
+    v_lambda_pos=0;
+    v_var=0;
+    while((p_lambda[v_var]<=e_lambda)&&(v_var<4)){
+        v_lambda_pos=v_var;
+        v_var++;
+    }
+    printf("O valor:%.2f\n",s_saida[v_lambda_pos][v_rpm_pos]);
+
+    return 0;
+}
